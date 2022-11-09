d9698d4fa6d6fad99c01c963337f7b1440502c69
AMinjon <x.aminjon@gmail.com>
2022-11-09 12:10:01 +0500

    shopbot

---
+++ keyboards/inline.py
@@ -0,0 +1,51 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from data.loader import db
+
+
+def generate_products_pagination(category_name, pagination=1):
+    markup = InlineKeyboardMarkup()
+    limit = 5
+    count = db.get_count_products_in_category(category_name)  # 14
+    max_pages = count // limit if count % limit == 0 else count // limit + 1
+    offset = 0 if pagination == 1 else (pagination - 1) * limit
+    products = db.get_products_to_page(category_name, offset, limit)
+    # [(1, 'Super Free', 123123, asdasda, 1), ()]
+    for product in products:
+        product_id, product_name = product[0], product[1]
+        btn = InlineKeyboardButton(text=product_name, callback_data=f'product_{product_id}')
+        markup.add(btn)
+
+    if pagination == 1:
+        page = InlineKeyboardButton(text=str(pagination), callback_data=f'page_{category_name}')
+        next_page = InlineKeyboardButton(text='⏭', callback_data='next')
+        markup.row(page, next_page)
+    elif 1 < pagination < max_pages:
+        prev_page = InlineKeyboardButton(text='⏮', callback_data='prev')
+        page = InlineKeyboardButton(text=str(pagination), callback_data=f'page_{category_name}')
+        next_page = InlineKeyboardButton(text='⏭', callback_data='next')
+        markup.row(prev_page, page, next_page)
+    elif pagination == max_pages:
+        prev_page = InlineKeyboardButton(text='⏮', callback_data='prev')
+        page = InlineKeyboardButton(text=str(pagination), callback_data=f'page_{category_name}')
+        markup.row(prev_page, page)
+    back_btn = InlineKeyboardButton(text='Назад', callback_data='back_categories')
+    main_menu = InlineKeyboardButton(text='Главное меню', callback_data='main_menu')
+    markup.row(back_btn, main_menu)
+    return markup
+
+
+def generate_product_detail(category_id, product_id, quantity=1):
+    markup = InlineKeyboardMarkup()
+    minus_btn = InlineKeyboardButton(text='➖', callback_data='minus')
+    quan_btn = InlineKeyboardButton(text=str(quantity), callback_data='quantity')
+    plus_btn = InlineKeyboardButton(text='➕', callback_data='plus')
+
+    add_cart = InlineKeyboardButton(text='В корзину 💵', callback_data=f'cart_{product_id}')
+    cart = InlineKeyboardButton(text='Корзину 🛒', callback_data='show_cart')
+    back = InlineKeyboardButton(text='Назад', callback_data=f'back_category_{category_id}')
+    main_menu = InlineKeyboardButton(text='Главное меню', callback_data='main_menu')
+
+    markup.row(minus_btn, quan_btn, plus_btn)
+    markup.row(add_cart, cart)
+    markup.row(back, main_menu)
+    return markup
---
+++ states/states.py
@@ -0,0 +1,10 @@
+from telebot.handler_backends import State, StatesGroup
+
+
+
+class CartStates(StatesGroup):
+    cart = State()
+    
+    
+    
+    
